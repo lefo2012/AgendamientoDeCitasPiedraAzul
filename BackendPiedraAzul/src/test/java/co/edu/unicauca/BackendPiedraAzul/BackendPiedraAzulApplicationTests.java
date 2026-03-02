97a46992cd3c4d2d24886f730c3e6ebd7e8d1910
LEFO <luisfierroor@gmail.com>
2026-03-01 21:08:16 -0500

    Estructura del proyecto

---
+++ BackendPiedraAzul/src/test/java/co/edu/unicauca/BackendPiedraAzul/BackendPiedraAzulApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.BackendPiedraAzul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPiedraAzulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
