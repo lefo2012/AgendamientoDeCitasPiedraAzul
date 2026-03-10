fed1cad45b1945e56551c6e2b1bd35ea48e3bf47
LuisaFerPas <luisafp@unicauca.edu.co>
2026-03-09 23:22:32 -0500

    feat: configured properties of database

--- BackendPiedraAzul/src/main/java/Appointments/persistence/repository/MedicalHistoryRepositoryJPA.java
+++ BackendPiedraAzul/src/main/java/Appointments/persistence/repository/MedicalHistoryRepositoryJPA.java
@@ -2,6 +2,8 @@ package Appointments.persistence.repository;
 
 import Appointments.persistence.entities.MedicalHistoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MedicalHistoryRepositoryJPA extends JpaRepository<MedicalHistoryEntity,Long> {
 }
--- BackendPiedraAzul/src/main/java/Appointments/persistence/repository/ScheduleRepositoryJPA.java
+++ BackendPiedraAzul/src/main/java/Appointments/persistence/repository/ScheduleRepositoryJPA.java
@@ -2,6 +2,8 @@ package Appointments.persistence.repository;
 
 import Appointments.persistence.entities.ScheduleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ScheduleRepositoryJPA extends JpaRepository<ScheduleEntity,Long> {
 }
