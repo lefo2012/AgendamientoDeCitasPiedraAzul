d27f6ab8961c8ea76cf630c0e26af720a6069e0e
Nelson Lopez <nelsonrodrigo@unicauca.edu.co>
2026-03-12 20:58:01 -0500

    feat: new branch FrontEnd created

--- FrontendPiedraAzul/src/app/app.routes.ts
+++ FrontendPiedraAzul/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from './layout/main-layout/main-layout';
+import { AppointmentList } from './features/appointments/pages/appointment-list/appointment-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: '',
+        component: AppointmentList
+      }
+    ]
+  }
+
+];
---
+++ FrontendPiedraAzul/src/app/layout/layout-module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,RouterOutlet
+]
+})
+export class LayoutModule { }
