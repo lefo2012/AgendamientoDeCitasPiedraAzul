685a0d24999fe8ca5c78a2d13971954025548c72
Nelson Lopez <nelsonrodrigo@unicauca.edu.co>
2026-03-24 20:04:49 -0500

    feat: update appointment components and styles for improved layout and functionality

--- BackendPiedraAzul/src/main/java/co/edu/unicauca/BackendPiedraAzul/Users/persistence/repository/DoctorRepositoryJPA.java
+++ BackendPiedraAzul/src/main/java/co/edu/unicauca/BackendPiedraAzul/Users/persistence/repository/DoctorRepositoryJPA.java
@@ -3,6 +3,7 @@ package co.edu.unicauca.BackendPiedraAzul.Users.persistence.repository;
 import co.edu.unicauca.BackendPiedraAzul.Appointments.domain.SpecialtyEnum;
 import co.edu.unicauca.BackendPiedraAzul.Users.persistence.entities.DoctorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,6 +14,7 @@ public interface DoctorRepositoryJPA extends JpaRepository<DoctorEntity,Long> {
 
     List<DoctorEntity> findBySpecialtiesContaining(SpecialtyEnum specialty);
 
+    @Query("SELECT d FROM DoctorEntity d WHERE d.user.email = :email")
     Optional <DoctorEntity> findByEmail(String email) throws Exception;
 
 }
