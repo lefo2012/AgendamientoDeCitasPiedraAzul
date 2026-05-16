f1561a49105d63c1fcd69686358dae9b01d9af5b
Nelson Lopez <nelsonrodrigo@unicauca.edu.co>
2026-05-16 18:44:49 -0500

    feat: add Keycloak configuration and environment variables for improved security and flexibility in the backend

--- BackendPiedraAzul/src/main/java/co/edu/unicauca/BackendPiedraAzul/Authentication/keycloak/KeycloakProvider.java
+++ BackendPiedraAzul/src/main/java/co/edu/unicauca/BackendPiedraAzul/Authentication/keycloak/KeycloakProvider.java
@@ -6,34 +6,55 @@ import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
 import org.keycloak.admin.client.resource.RealmResource;
 import org.keycloak.admin.client.resource.UsersResource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class KeycloakProvider {
 
-    private static final String SERVER_URL = "http://localhost:8080";
-    private static final String REALM_NAME = "piedraAzul-dev";
-    private static final String REALM_MASTER = "master";
-    private static final String ADMIN_CLI = "admin-cli";
-    private static final String USER_CONSOLE = "admin";
-    private static final String PASSWORD_CONSOLE = "1234";
-    private static final String CLIENT_SECRET = "HFn9D3q4cLaZyLfTcs7h4J4cDLLLaRLh";
+    private final String serverUrl;
+    private final String realmName;
+    private final String realmMaster;
+    private final String adminCli;
+    private final String adminUsername;
+    private final String adminPassword;
+    private final String clientSecret;
 
-    public static RealmResource getRealmResource() {
+    public KeycloakProvider(
+            @Value("${keycloak.admin.server-url}") String serverUrl,
+            @Value("${keycloak.admin.realm}") String realmName,
+            @Value("${keycloak.admin.master-realm}") String realmMaster,
+            @Value("${keycloak.admin.client-id}") String adminCli,
+            @Value("${keycloak.admin.username}") String adminUsername,
+            @Value("${keycloak.admin.password}") String adminPassword,
+            @Value("${keycloak.admin.client-secret}") String clientSecret
+    ) {
+        this.serverUrl = serverUrl;
+        this.realmName = realmName;
+        this.realmMaster = realmMaster;
+        this.adminCli = adminCli;
+        this.adminUsername = adminUsername;
+        this.adminPassword = adminPassword;
+        this.clientSecret = clientSecret;
+    }
+
+    public RealmResource getRealmResource() {
         Keycloak keycloak = KeycloakBuilder.builder()
-                .serverUrl(SERVER_URL)
-                .realm(REALM_MASTER)
-                .clientId(ADMIN_CLI)
-                .username(USER_CONSOLE)
-                .password(PASSWORD_CONSOLE)
-                .clientSecret(CLIENT_SECRET)
+                .serverUrl(serverUrl)
+                .realm(realmMaster)
+                .clientId(adminCli)
+                .username(adminUsername)
+                .password(adminPassword)
+                .clientSecret(clientSecret)
                 .resteasyClient(new ResteasyClientBuilderImpl()
                         .connectionPoolSize(10)
                         .build())
                 .build();
 
-        return keycloak.realm(REALM_NAME);
+        return keycloak.realm(realmName);
     }
 
-    public static UsersResource getUserResource() {
+    public UsersResource getUserResource() {
         RealmResource realmResource = getRealmResource();
         return realmResource.users();
     }
