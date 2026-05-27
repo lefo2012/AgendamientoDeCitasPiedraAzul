27779c36daef9088a72264e9078e5227e2dcef02
Nelson Lopez <nelsonrodrigo@unicauca.edu.co>
2026-05-26 21:14:09 -0500

    feat: add authentication guard to appointment and admin routes

--- FrontEndMedicos/src/app/app.routes.ts
+++ FrontEndMedicos/src/app/app.routes.ts
@@ -13,7 +13,7 @@ export const routes: Routes = [
 					import('./features/main/pages/main/main').then((m) => m.Main),
 			},
 			{
-				path: 'citas',
+				path: 'citas',			
 				redirectTo: 'citas/agendar',
 				pathMatch: 'full',
 			},
@@ -25,31 +25,37 @@ export const routes: Routes = [
 			},
 			{
 				path: 'citas/nueva',
+				canActivate: [authRequiredGuard],
 				loadComponent: () =>
 					import('./features/appointments/pages/create-appointment/create-appointment').then((m) => m.CreateAppointment),
 			},
 			{
 				path: 'citas/reagendar/:id',
+				canActivate: [authRequiredGuard],
 				loadComponent: () =>
 					import('./features/appointments/pages/reschedule-appointment/reschedule-appointment').then((m) => m.RescheduleAppointment),
 			},
 			{
 				path: 'admin',
+				canActivate: [authRequiredGuard],
 				loadComponent: () =>
 					import('./features/users/pages/admin-home/admin-home').then((m) => m.AdminHome),
 			},
 			{
 				path: 'admin/registrar-medico',
+				canActivate: [authRequiredGuard],
 				loadComponent: () =>
 					import('./features/users/pages/register-user/register-user').then((m) => m.RegisterUser),
 			},
 			{
 				path: 'admin/configuracion',
+				canActivate: [authRequiredGuard],
 				loadComponent: () =>
 					import('./features/users/pages/configure-doctors/configure-doctors').then((m) => m.ConfigureDoctors),
 			},		
 			{
 				path: 'admin/editar-medico/:id',
+				canActivate: [authRequiredGuard],
 				loadComponent: () =>
 					import('./features/users/pages/update-doctor/update-doctor').then((m) => m.UpdateDoctor),
 			}
@@ -67,11 +73,13 @@ export const routes: Routes = [
 			},
 			{
 				path: 'register',
+				canActivate: [authRequiredGuard],
 				loadComponent: () =>
 					import('./features/users/pages/register-user/register-user').then((m) => m.RegisterUser),
 			},
 			{
 				path: 'configure-schedule',
+				canActivate: [authRequiredGuard],
 				loadComponent: () =>
 					import('./features/users/pages/configure-schedule-doctor/configure-schedule-doctor').then((m) => m.ConfigureScheduleDoctor),
 			},
